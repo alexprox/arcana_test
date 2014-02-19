89a999a3b4c730d69aa343ae9ada36356b682b37
Alex <alexprox91@gmail.com>
2014-02-19 16:23:27 +0200

    Home Controller updated
    Tweet getter added

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -6,8 +6,25 @@ class HomeController extends BaseController {
         if(!Auth::check()) {
             $this->layout->content = View::make('Home/home');
         } else {
-            $this->layout->content = View::make('Home/feed');
+            $this->layout->content = View::make('Home/feed', array(
+                'tweets' => $this->get_tweets()
+            ));
         }
     }
-
+    
+    public function getTweets() {
+        $tweets = array();
+        if(Auth::check()) {
+            $tweets = $this->get_tweets();
+        }
+        return Response::json(array('tweets' => $tweets));
+    }
+    
+    private function get_tweets() {
+        $foll_ids = array(Auth::user()->id);
+        foreach(Auth::user()->following()->get() as $foll_user) {
+            $foll_ids[] = $foll_user->id;
+        }
+        return Tweet::whereIn('author_id', $foll_ids)->orderBy('created_at')->get();
+    }
 }
