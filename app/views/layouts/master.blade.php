9d4dd41752c21ac9260d1cc11cad1f0f57c87ccf
Alex <alexprox91@gmail.com>
2014-02-19 20:31:41 +0200

    Reply modal added

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -24,6 +24,27 @@
             @yield('content')
             @include('footer')
         </div>
+        <div class="modal fade reply-modal" tabindex="-1" role="dialog" aria-labelledby="Reply" aria-hidden="true">
+            <div class="modal-dialog modal-sm">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                        <h4 class="modal-title" id="myModalLabel">Reply</h4>
+                    </div>
+                    {{ Form::open(array('route' => 'replyTweet', 'class' => 'form-horizontal-no-label')) }}
+                        <div class="modal-body">
+                            {{ Form::hidden('tweet_id') }}
+                            {{ Form::textarea('tweet', NULL, array("class"=>"form-control", "placeholder"=>"Write reply", "rows" => 1)) }}
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            <span class="counter badge pull-left" for="tweet" max="140">140</span>
+                            {{ Form::submit('Tweet', array("class"=>"btn btn-primary pull-right ajax")) }}
+                        </div>
+                    {{ Form::close() }}
+                </div>
+            </div>
+        </div>
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         {{ HTML::script('js/jquery.min.js') }}
         <!-- Include all compiled plugins (below), or include individual files as needed -->
